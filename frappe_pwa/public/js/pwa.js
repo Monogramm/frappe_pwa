9ab6f61e31d893ce7a3e741055de35491abbcad8
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-06-06 22:58:58 +0200

    🙈 Do not ignore public files
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

---
+++ frappe_pwa/public/js/pwa.js
@@ -0,0 +1,139 @@
+/*!
+ * Frappe PWA (https://github.com/Monogramm/frappe_pwa)
+ * Copyright 2020 Monogramm
+ * Licensed under AGPL v3 (https://github.com/Monogramm/frappe_pwa/blob/master/LICENSE)
+ */
+
+if ('serviceWorker' in navigator) {
+    //window.vapidPublicKey = new Uint8Array('{{ vapid_public_key }}');
+
+    window.addEventListener('load', function () {
+        if (navigator.serviceWorker.controller) {
+            console.log('[PWA] ServiceWorker found, no need to register');
+        } else {
+            navigator.serviceWorker.register('/sw.js').then(
+                function (registration) {
+                    // Registration was successful
+                    console.log('[PWA] ServiceWorker registration successful with scope: ' + registration.scope);
+
+                    if (window.vapidPublicKey) {
+                        console.log('[PWA] Subscribing to push notifications');
+
+                        registration.pushManager.subscribe({
+                            userVisibleOnly: true,
+                            applicationServerKey: window.vapidPublicKey
+                        }).then(
+                            function (pushSubscription) {
+                                console.log(pushSubscription.subscriptionId);
+                                console.log(pushSubscription.endpoint);
+                                // The push subscription details needed by the application
+                                // server are now available, and can be sent to it using,
+                                // for example, an XMLHttpRequest.
+                            }, function (error) {
+                                // During development it often helps to log errors to the
+                                // console. In a production environment it might make sense to
+                                // also report information about errors back to the
+                                // application server.
+                                console.log(error);
+                            }
+                        );
+                    }
+                }, function (err) {
+                    // registration failed :(
+                    console.log('[PWA] ServiceWorker registration failed: ', err);
+                });
+        }
+    });
+
+    var installPromptEvent = null;
+    window.addEventListener('beforeinstallprompt', function (e) {
+        e.preventDefault();
+        installPromptEvent = e;
+
+        console.log('[PWA] Application could be installed');
+        showAddToHomeScreen();
+    });
+
+    function showAddToHomeScreen(event) {
+        btnAdd = document.getElementById('pwa-install-link');
+
+        if (installPromptEvent === null) {
+            console.log('[PWA] No A2HS event stored for this device');
+        } else if (btnAdd === null) {
+            console.log('[PWA] The page has not finished initializing. Postponing A2HS on page load.');
+            document.body.addEventListener('load', showAddToHomeScreen);
+        } else {
+            // Update UI to notify the user they can add to home screen
+            btnAdd.classList.remove('is-hidden');
+
+            btnAdd.addEventListener('click', addToHomeScreen);
+        }
+    }
+
+    function addToHomeScreen(event) {
+        // Show the prompt
+        hideAddToHomeScreen();
+
+        if (installPromptEvent === null) {
+            console.log('[PWA] No A2HS event to trigger for this device');
+        } else {
+            installPromptEvent.prompt();
+
+            // Wait for the user to respond to the prompt
+            installPromptEvent.userChoice
+                .then(function (choiceResult) {
+                    if (choiceResult.outcome === 'accepted') {
+                        console.log('[PWA] User accepted the A2HS prompt');
+                    } else {
+                        console.log('[PWA] User dismissed the A2HS prompt');
+                    }
+                    installPromptEvent = null;
+                });
+        }
+    }
+
+    function hideAddToHomeScreen(event) {
+        btnAdd = document.getElementById('pwa-install-link');
+
+        // hide our user interface that shows our A2HS button
+        if (btnAdd !== null) {
+            btnAdd.classList.add('is-hidden');
+        }
+    }
+
+    window.addEventListener('appinstalled', hideAddToHomeScreen);
+
+    // Push notifications
+    function pushNotification(message) {
+        navigator.serviceWorker.ready
+            .then(function (serviceWorkerRegistration)  {
+                serviceWorkerRegistration.pushManager.getSubscription()
+                    .then(function (subscription) {
+                        $.post('/push', {
+                            subscription: subscription.toJSON(),
+                            message: message
+                        });
+                    });
+            });
+    }
+
+    // Request to allow push notifications
+    if (window.vapidPublicKey) {
+        if (!("Notification" in window)) {
+            console.error("[PWA] This browser does not support desktop notification");
+        } else if (Notification.permission === "granted") {
+            console.log("[PWA] Permission to receive notifications has been granted");
+        } else if (Notification.permission !== 'denied') {
+            Notification.requestPermission(function (permission) {
+                // If the user accepts, let's create a notification
+                if (permission === "granted") {
+                    console.log("[PWA] Permission to receive notifications has been granted");
+                }
+            });
+        }
+    }
+} else if(window.location.protocol !== 'https:') {
+    console.warn('[PWA] Requires secure HTTPS connection');
+} else {
+    console.warn('[PWA] No Service Worker support on your device');
+}
---
+++ frappe_pwa/public/js/sw.js
@@ -0,0 +1,143 @@
+/*!
+ * Frappe PWA (https://github.com/Monogramm/frappe_pwa)
+ * Copyright 2020 Monogramm
+ * Licensed under AGPL v3 (https://github.com/Monogramm/frappe_pwa/blob/master/LICENSE)
+ */
+
+// Service Worker
+var CACHE_NAME = 'frappe-cache-v1';
+var urlsToCache = [
+    '/',
+    '/website_script.js',
+    '/manifest.json',
+    '/assets/js/frappe-web.min.js',
+    '/assets/js/erpnext-web.min.js',
+    '/assets/frappe/js/lib/socket.io.min.js',
+    '/assets/frappe/js/lib/bootstrap.min.js',
+    '/assets/frappe/js/lib/jquery/jquery.min.js',
+];
+
+// Install stage sets up the index page (home page) in the cache and opens a new cache
+this.addEventListener('install', function (event) {
+    console.log('[SW] Install Event processing');
+
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function (cache) {
+                console.log('[SW] Installing cache ' + CACHE_NAME);
+                return cache.addAll(urlsToCache)
+                    .catch(function (err) {
+                        console.log('[SW] Cache install Failed: ' + err);
+                    });
+            }).catch(function (err) {
+                console.error(event.request.url, err);
+                console.log('[SW] Install Failed: ' + err);
+            })
+    );
+});
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+this.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                if (!response) {
+                    return handleNoCacheMatch(event);
+                }
+                // Update cache record in the background
+                fetchFromNetworkAndCache(event);
+                // Reply with stale data
+                return response
+            })
+    );
+});
+
+// When activating the Service Worker, clear older caches
+this.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (keys) {
+            return Promise.all(keys.map(function (key) {
+                clearCache(key);
+            }));
+        }));
+});
+
+// TODO Respond to 'push' events and trigger notifications on the registration
+//this.addEventListener('push', function (event) {
+//    let title = (event.data && event.data.text());
+//    let body = "We have received a push message";
+//    let tag = "push-alis-srv-prv-notification";
+//    let icon = '/build/images/alis-logo.png';
+//
+//    event.waitUntil(
+//        self.registration.showNotification(title, { body, icon, tag })
+//    );
+//});
+
+
+function fetchFromNetworkAndCache(event) {
+    // DevTools opening will trigger these o-i-c requests, which this SW can't handle.
+    // There's probaly more going on here, but I'd rather just ignore this problem. :)
+    // https://github.com/paulirish/caltrainschedule.io/issues/49
+    if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') {
+        return;
+    }
+
+    return fetch(event.request).then(function (res) {
+        // foreign requests may be res.type === 'opaque' and missing a url
+        if (!res.url) {
+            return res;
+        }
+        // Only cache GET requests
+        if (event.request.method !== 'GET') {
+            return res;
+        }
+        // regardless, we don't want to cache other origin's assets
+        if (new URL(res.url).origin !== location.origin) {
+            return res;
+        }
+
+        // If request was success, add or update it in the cache
+        updateCache(event.request, res.clone());
+        // TODO: figure out if the content is new and therefore the page needs a reload.
+
+        return res;
+    }).catch(function (err) {
+        console.error(event.request.url, err);
+        console.log('[SW] Network request Failed. Serving content from cache: ' + err);
+        return fromCache(event.request);
+    });
+}
+
+function handleNoCacheMatch(event) {
+    return fetchFromNetworkAndCache(event);
+}
+
+function fromCache(request) {
+    // Check to see if you have it in the cache
+    // Return response
+    // If not in the cache, then return error page
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            if (!matching || matching.status === 404) {
+                return Promise.reject("no-match");
+            }
+
+            return matching;
+        });
+    });
+}
+
+function updateCache(request, response) {
+    return caches.open(CACHE_NAME).then(function (cache) {
+        return cache.put(request, response);
+    });
+}
+
+function clearCache(key) {
+    if (key !== CACHE_NAME) {
+        console.log('[SW] Cleaning cache ' + key);
+        return caches.delete(key);
+    }
+}
