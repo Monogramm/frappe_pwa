21f027a6953d8eec2d468a7568f09834afca9472
Emil <amionv99@gmail.com>
2020-07-05 18:48:46 +0300

    :white_check_mark: Add test for configure_pwa
    Signed-off-by: Emil <amionv99@gmail.com>

--- frappe_pwa/frappe_pwa/doctype/web_app_manifest/test_web_app_manifest.py
+++ frappe_pwa/frappe_pwa/doctype/web_app_manifest/test_web_app_manifest.py
@@ -6,5 +6,13 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
+
 class TestWebAppManifest(unittest.TestCase):
-	pass
+    def setUp(self):
+        self.web_manifest = frappe.get_doc('Web App Manifest')
+
+    def test_configure_pwa(self):
+        self.web_manifest.configure_pwa()
+        ws = frappe.get_doc('Website Settings')
+        ws.head_html.remove('<link href="/assets/frappe_pwa/manifest.json" rel="manifest">')
+        ws.save()
