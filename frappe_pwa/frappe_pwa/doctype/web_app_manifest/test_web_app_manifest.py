65da0dde2ada05a5a282a87341d7861d6fcc497c
Mathieu Brunot <mathieu.brunot@monogramm.io>
2020-07-07 17:02:58 +0200

    Update frappe_pwa/frappe_pwa/doctype/web_app_manifest/test_web_app_manifest.py

--- frappe_pwa/frappe_pwa/doctype/web_app_manifest/test_web_app_manifest.py
+++ frappe_pwa/frappe_pwa/doctype/web_app_manifest/test_web_app_manifest.py
@@ -13,4 +13,4 @@ class TestWebAppManifest(unittest.TestCase):
     def test_configure_pwa(self):
         configure_pwa()
         ws = frappe.get_doc('Website Settings')
-        self.assertIn('''rel="manifest">''', ws.head_html)
+        self.assertIn('''rel="manifest"''', ws.head_html)
